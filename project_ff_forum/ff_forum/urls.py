0b0b629c69bb2f1a996c7d65caabb32ea2edd510
datny <robert.przydatek.cl@gmail.com>
2019-06-26 10:20:34 +0200

    add signup, login, logout

--- project_ff_forum/ff_forum/urls.py
+++ project_ff_forum/ff_forum/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from ff_forum.views import HomePageView
+from django.urls import path, include, re_path
+from ff_forum.views import HomePageView, TestPage, ThanksPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
     path('accounts/', include('accounts.urls', namespace="accounts",)),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', HomePageView.as_view(), name='home'),
+    path('test/', TestPage.as_view(), name='test'),
+    path('thanks/', ThanksPage.as_view(), name='thanks'),
 ]
